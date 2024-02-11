6d264aae459ebfb08b23d1beb4eafcbf782d1161
VaibhavRtd7 <vaibhavrathod1902@gmail.com>
2024-02-11 23:36:47 +0530

    custom-hook-creation

--- src/App.js
+++ src/App.js
@@ -2,15 +2,15 @@ import Search from "./components/Search"
 import Pagination from "./components/Pagination"
 import Stories from "./components/Stories"
 
-import { AppContext } from "./context"
-import { useContext } from "react"
+// import { AppContext } from "./context"
+// import { useContext } from "react"
+import { useGlobalContext } from "./context";
 
 const App = () => {   
-  const data = useContext(AppContext);
-
+  const data = useGlobalContext();
   return (
     <>
-      <div className="m-1 p-5 text-xl bg-cyan-200"> Welcome to My News Website  {data}!!</div>
+      <div className="m-1 p-5 text-3xl bg-cyan-200"> Welcome to My News Website :-   {data}!!</div>
        <Search />
        <Pagination />
        <Stories />
